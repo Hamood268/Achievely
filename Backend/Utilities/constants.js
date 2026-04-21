6f3728bbe99c9caf8303349065c46391aeee2edf
Mohammed <hastreams6@gmail.com>
2026-04-22 00:08:17 +0300

    Added OWNED_GAMES steam endpoint

--- Backend/Utilities/constants.js
+++ Backend/Utilities/constants.js
@@ -8,10 +8,12 @@ module.exports = {
     STEAM: {
         "PLAYER_DATA": "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/",
         'RECENTLY_PLAYED': "https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/",
+        'OWNED_GAMES': "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/",
         'ACHIEVEMENTS': "https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/",
         'ACHIEVEMENTS_2': "https://api.steampowered.com/IPlayerService/GetGameAchievements/v1/",
         'USER_ACHIEVEMENTS': "https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/",
         'ACHIEVEMENT_PERCENTAGE': "https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v2/",
+        'APP_DETAILS': "https://store.steampowered.com/api/appdetails"
     },
 
     STEAMGRID: {
