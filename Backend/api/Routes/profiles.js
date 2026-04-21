c126aad6ec17f60c1d21110428683e0aaaeadf98
Mohammed <hastreams6@gmail.com>
2026-04-22 00:07:27 +0300

    Added Owned Games endpoint

--- Backend/api/Routes/profiles.js
+++ Backend/api/Routes/profiles.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
     profiles,
-    profile_lastplayed
+    profile_lastplayed,
+    profile_ownedgames
 } = require("../Controllers/profiles");
 
 router.get('/users/:steamId/profile', profiles)
 router.get('/users/:steamId/games', profile_lastplayed)
+router.get('/users/:steamId/games/owned', profile_ownedgames)
 
 module.exports = router;
